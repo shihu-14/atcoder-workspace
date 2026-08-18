978002e25f4410b4c75a193f9a91c838b194381a
shihu-14 <methacrylicacid86@gmail.com>
2026-08-18 18:42:48 +0900

    feat: abc098

--- src/abc098_d.cpp
+++ src/abc098_d.cpp
@@ -6,12 +6,25 @@ using namespace std;
 int main()
 {
     int n; cin >> n;
-    vector<int> a(n), s1(n+1), s2(n+1);
+    vector<int> a(n);
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        s1[i+1] = s1[i]^a[i];
-        s2[i+1] = s2[i]+a[i];
     }
+    long long ans = 0;
+    int sum = 0, r = 0;
+    for (int l = 0; l < n; l++)
+    {
+        while(r < n && (sum&a[r]) == 0)
+        {
+            // cout << l << " " << r << " " << sum << "vs" << (sum^a[r]) << '\n';
+            sum ^= a[r];
+            r++;
+        }
+        // cout << l << " " << r << " " << sum << '\n';
+        ans += r-l;
+        sum ^= a[l];
+    }
+    cout << ans << '\n';
     return 0;
 }
